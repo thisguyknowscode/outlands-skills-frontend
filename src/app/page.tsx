1ccc8e049fcd74be4cb4f26109ef69accfa4e2b7
Michael Snyder <thisguyknowscode@gmail.com>
2024-11-20 13:30:39 -0500

    Set up extremely basic front-end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,69 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import styled from '@emotion/styled'
+import { Box, Button, Paper, TextField } from '@mui/material'
+import React, { useState } from 'react'
+import Markdown from 'react-markdown'
+
+const StyledPaper = styled(Paper)`
+	padding: 2rem;
+	margin: 2rem;
+`
+
+const StyledResponseContainer = styled(Box)`
+	border: 2px solid #999;
+	border-radius: 0.25rem;
+	padding: 1rem;
+`
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	const [question, setQuestion] = useState('')
+	const [answer, setAnswer] = useState('')
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	const questionHandler = (ev: any) => {
+		ev.preventDefault()
+
+		setQuestion(ev.target.value)
+	}
+
+	const searchClickHandler = async () => {
+		const resp = await fetch('http://localhost:8080/dev/chat', {
+			method: 'POST',
+			body: JSON.stringify({ query: question })
+		})
+
+		const respJson = await resp.json()
+
+		if (respJson?.length) {
+			setAnswer(respJson[0].response)
+		}
+	}
+
+	const resetClickHandler = () => {
+		setQuestion('')
+	}
+
+	return (
+		<Box>
+			<StyledPaper elevation={5}>
+				<StyledResponseContainer>
+					<Markdown>{answer}</Markdown>
+				</StyledResponseContainer>
+			</StyledPaper>
+			<StyledPaper elevation={5}>
+				<Box>
+					<TextField
+						label="Enter your question about Outlands"
+						onChange={questionHandler}
+						fullWidth
+						value={question}
+					/>
+				</Box>
+				<Box>
+					<Button onClick={searchClickHandler}>Search</Button>
+					<Button onClick={resetClickHandler}>Reset</Button>
+				</Box>
+			</StyledPaper>
+		</Box>
+	)
 }
